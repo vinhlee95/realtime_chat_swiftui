d7aa01095642f0b10c2869220df477520098d103
Vinh Le <lethanhvinh95@gmail.com>
2022-07-29 08:22:17 +0300

    Initial Commit

---
+++ realtime_chat/realtime_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  realtime_chatApp.swift
+//  realtime_chat
+//
+//  Created by Vinh Le on 29.7.2022.
+//
+
+import SwiftUI
+
+@main
+struct realtime_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
