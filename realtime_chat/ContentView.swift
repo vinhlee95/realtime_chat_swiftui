be85299c23dbc2fd0102216198b0774ecc482916
Vinh Le <lethanhvinh95@gmail.com>
2022-07-30 10:45:24 +0300

    initial commit

--- realtime_chat/ContentView.swift
+++ realtime_chat/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                    Text("Login").tag(true)
+                    Text("Create account").tag(false)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(5)
+                
+                Image(systemName: "person.fill")
+                    .font(.system(size: 64))
+                    .padding()
+                
+                TextField("Email", text: $email).padding()
+                SecureField("Password", text: $password).padding()
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Create account")
+                            .foregroundColor(Color.white)
+                            .padding(.vertical, 10)
+                        Spacer()
+                    }.background(Color.blue)
+                }.cornerRadius(4)
+
+                
+                
+                
+                Text("Scroll view content")
+            }
+            .navigationTitle("Create account")
+            .padding(.horizontal, 20)
+        }
     }
 }
 
