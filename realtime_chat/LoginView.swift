2e2a326fbd12d54572c9e1f1adc264333564a182
Vinh Le <lethanhvinh95@gmail.com>
2022-07-30 11:10:42 +0300

    add handle authenticate button

---
+++ realtime_chat/LoginView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  realtime_chat
+//
+//  Created by Vinh Le on 29.7.2022.
+//
+
+import SwiftUI
+
+enum LoginMode {
+    case login
+    case createNewAccount
+}
+
+struct LoginView: View {
+    @State var mode = LoginMode.login
+    @State var email = ""
+    @State var password = ""
+        
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                Picker(selection: $mode, label: Text("Picker here")) {
+                    Text("Login").tag(LoginMode.login)
+                    Text("Create account").tag(LoginMode.createNewAccount)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(5)
+                
+                Image(systemName: "person.fill")
+                    .font(.system(size: 64))
+                    .padding()
+                
+                TextField("Email", text: $email).padding().keyboardType(.emailAddress).autocapitalization(.none).background(.white).cornerRadius(4)
+                SecureField("Password", text: $password).padding().background(.white).cornerRadius(4)
+                
+                Button {
+                    handleAuthenticate()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text(mode == LoginMode.login ? "Login" : "Create account")
+                            .foregroundColor(Color.white)
+                            .padding(.vertical, 10)
+                        Spacer()
+                    }.background(Color.blue)
+                }.cornerRadius(4)
+            }
+            .navigationTitle(mode == LoginMode.login ? "Login" : "Create new account")
+            .padding(.horizontal, 20)
+            .background(Color(.init(white: 0, alpha: 0.05)))
+        }
+    }
+    
+    private func handleAuthenticate() {
+        if mode == LoginMode.login {
+            print("logging in to your account")
+        } else if mode == LoginMode.createNewAccount {
+            print("create new account")
+        } else {
+            print("no action")
+        }
+        
+    }
+}
+
+struct ContentView_Previews1: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
