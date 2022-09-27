39de36e6ab553c8a7b8390ec2f0c64b6b6e6bebb
Vinh Le <lethanhvinh95@gmail.com>
2022-09-27 07:24:32 +0300

    styling for message list

--- realtime_chat/PrivateChat/PrivateChatView.swift
+++ realtime_chat/PrivateChat/PrivateChatView.swift
@@ -13,17 +13,37 @@ struct PrivateChatView: View {
     var body: some View {
             ScrollView {
                 ForEach(0..<10) { num in
-                    Text("FAKE MESSAGE FOR NOW")
+                    HStack {
+                        Spacer()
+                        HStack {
+                            Text("FAKE MESSAGE FOR NOW")
+                                .foregroundColor(.white)
+                                .padding()
+                        }
+                        .background(.blue)
+                        .cornerRadius(8)
+                    }
+                    .padding(.horizontal, 8)
+                }
+                
+                HStack {
+                    Spacer()
                 }
             }
-            .navigationTitle("Chat")
+            .navigationTitle(chatUser?.email ?? "")
+            .navigationBarTitleDisplayMode(.inline)
+            .background(Color(.init(white: 0.95, alpha: 1)))
         }
 }
 
 struct PrivateChatView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            PrivateChatView(chatUser: nil)
+            PrivateChatView(chatUser: .init(data: [
+                "email": "test@test.com",
+                "id": "rXU3I3ki0fbMD8btWJtoF8rRLXf1",
+                "profileImageUrl": "https://firebasestorage.googleapis.com:443/v0/b/swiftui-realtime-chat.appspot.com/o/images%2Fprofile%2FrXU3I3ki0fbMD8btWJtoF8rRLXf1.jpg?alt=media&token=d6267fc7-4761-4d67-b56a-a235fd486b96"
+            ]))
         }
     }
 }
