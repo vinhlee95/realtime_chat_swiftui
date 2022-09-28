191a77875b50e7211ae5a76aca11214c1f88fd7c
Vinh Le <lethanhvinh95@gmail.com>
2022-09-28 07:46:41 +0300

    render chat text form

--- realtime_chat/PrivateChat/PrivateChatView.swift
+++ realtime_chat/PrivateChat/PrivateChatView.swift
@@ -9,31 +9,60 @@ import SwiftUI
 
 struct PrivateChatView: View {
     let chatUser: ChatUser?
+    @State private var chatText: String = ""
     
     var body: some View {
-            ScrollView {
-                ForEach(0..<10) { num in
-                    HStack {
-                        Spacer()
-                        HStack {
-                            Text("FAKE MESSAGE FOR NOW")
-                                .foregroundColor(.white)
-                                .padding()
-                        }
-                        .background(.blue)
-                        .cornerRadius(8)
-                    }
-                    .padding(.horizontal, 8)
-                }
-                
+        VStack {
+            chatList
+            chatForm
+        }
+        .navigationTitle(chatUser?.email ?? "")
+        .navigationBarTitleDisplayMode(.inline)
+        // fix the issue where the ScrollView blend to the nav bar
+        .padding(.top, 1)
+    }
+    
+    private var chatList: some View {
+        ScrollView {
+            Spacer().padding(.top, 4)
+            ForEach(0..<20) { num in
                 HStack {
                     Spacer()
+                    HStack {
+                        Text("FAKE MESSAGE FOR NOW")
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    .background(.blue)
+                    .cornerRadius(8)
                 }
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
             }
-            .navigationTitle(chatUser?.email ?? "")
-            .navigationBarTitleDisplayMode(.inline)
-            .background(Color(.init(white: 0.95, alpha: 1)))
-        }
+            
+            HStack {
+                Spacer()
+            }
+        }.background(Color(.init(white: 0.95, alpha: 1)))
+    }
+    
+    private var chatForm: some View {
+        HStack {
+            Image(systemName: "photo").padding(.horizontal, 8)
+            // Render placeholder for TextEditor since it does not have one by default
+            ZStack {
+                HStack {
+                    Text("Description").padding(.leading, 4)
+                    Spacer()
+                }
+                TextEditor(text: $chatText).frame(height: 40).opacity(chatText.isEmpty ? 0.5 : 1)
+            }
+            Button {
+            } label: {
+                Text("Send").foregroundColor(.white).padding(.horizontal, 16).padding(.vertical, 8)
+            }.background(.blue).cornerRadius(4)
+        }.padding(.vertical, 8).padding(.horizontal, 12)
+    }
 }
 
 struct PrivateChatView_Previews: PreviewProvider {
